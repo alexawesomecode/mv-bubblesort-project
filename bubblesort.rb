a632fa56f4a4261a6148da3521b9b52716796f06
alex <alexawesomecode@gmail.com>
2019-10-30 18:04:46 -0300

    some progress

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,3 +1,14 @@
-def bubble_sort
+def bubble_sort(x)
+    sorted = 0
+    index = 0
+    while index < x.size
+        if x[index] > x[index + 1]
+            x[index + 1] = x[index] 
+            x[index] = x[index + 1]
+        end 
+    index=+1
+    end
+    return x
 end
 
+puts bubble_sort([1,2,3,3,6,2])
