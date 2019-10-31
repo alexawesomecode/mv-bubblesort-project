857e08c8fac147c97d2dbdc4439f473dcc194bab
Lucasdfg <cervantes.lucas99@gmail.com>
2019-10-31 17:06:25 -0600

    added 2nd assignment

--- bubblesort.rb
+++ bubblesort.rb
@@ -27,10 +27,24 @@ puts bubble_sort([4,3,78,2,0,2])
 
 
 
-def bubble_sort_by(a){
-    bubble_sort(a)
-}
-bubble_sort_by([4,3,78,2,0,2])
+def bubble_sort_by(arr)
+    arr.each do |_item|
+        index = 0
+        while index < arr.size - 1
+            if yield(arr[index], arr[index+1])
+                arr[index], arr[index+1] = arr[index+1], arr[index]
+            end
+            index+=1
+        end 
+    end
+
+    arr
+end
+
+r = bubble_sort_by([4,3,78,2,0,2]) do |left, right|
+    left>right
+end
+puts r.to_s
 
 
 
--- test.rb
+++ test.rb
@@ -1,9 +1,10 @@
 
 
 
-def bubble_sort_by(a){
-    
-}
+r = bubble_sort_by([4,3,78,2,0,2]) do |left, right|
+    left>right
+end
+puts r.to_s
 
 
 
