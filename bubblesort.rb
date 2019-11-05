b3007b15f4c65327b383c82752ddf361a55eaf75
Lucasdfg <cervantes.lucas99@gmail.com>
2019-11-05 13:35:37 -0600

    fixed code review

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,25 +1,14 @@
 def bubble_sort(x)
     x.each do |item|
-    
         index = 0
     while index < x.size - 1
-    
         if x[index] > x[index + 1]
-     
-            x[index], x[index + 1] = x[index + 1], x[index]
-    
-    
+        x[index], x[index + 1] = x[index + 1], x[index]
         end 
-    
         index+=1
-    
+        end
     end
-    
-end
-
-    
-x
-
+    x
 end
 
 puts bubble_sort([4,3,78,2,0,2])
@@ -31,7 +20,7 @@ def bubble_sort_by(arr)
     arr.each do |_item|
         index = 0
         while index < arr.size - 1
-            if yield(arr[index], arr[index+1])
+            if yield(arr[index], arr[index+1] ) >= 1
                 arr[index], arr[index+1] = arr[index+1], arr[index]
             end
             index+=1
@@ -41,8 +30,8 @@ def bubble_sort_by(arr)
     arr
 end
 
-r = bubble_sort_by([4,3,78,2,0,2]) do |left, right|
-    left>right
+r = bubble_sort_by(["hi","hello","hey", "hola", "comoestas"]) do |left, right|
+    left.length - right.length
 end
 puts r.to_s
 
