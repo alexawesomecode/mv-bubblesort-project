35b2b684c8e610453a9379bc4b2cf4bea1db8cf5
alex <alexawesomecode@gmail.com>
2019-11-05 16:39:05 -0300

    fixed rubocop

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,44 +1,29 @@
 def bubble_sort(x)
-    x.each do |item|
-        index = 0
+  x.each do |_item|
+    index = 0
     while index < x.size - 1
-        if x[index] > x[index + 1]
-        x[index], x[index + 1] = x[index + 1], x[index]
-        end 
-        index+=1
-        end
+      x[index], x[index + 1] = x[index + 1], x[index] if x[index] > x[index + 1]
+      index += 1
     end
-    x
+  end
+  x
 end
 
-puts bubble_sort([4,3,78,2,0,2])
-
-
-
+puts bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(arr)
-    arr.each do |_item|
-        index = 0
-        while index < arr.size - 1
-            if yield(arr[index], arr[index+1] ) >= 1
-                arr[index], arr[index+1] = arr[index+1], arr[index]
-            end
-            index+=1
-        end 
+  arr.each do |_item|
+    index = 0
+    while index < arr.size - 1
+      arr[index], arr[index + 1] = arr[index + 1], arr[index] if yield(arr[index], arr[index + 1]) >= 1
+      index += 1
     end
+  end
 
-    arr
+  arr
 end
 
-r = bubble_sort_by(["hi","hello","hey", "hola", "comoestas"]) do |left, right|
-    left.length - right.length
+r = bubble_sort_by(%w[hi hello hey hola comoestas]) do |left, right|
+  left.length - right.length
 end
 puts r.to_s
-
-
-
-
-
-
-
-
